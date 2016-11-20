aa58f456951c4caa6c2211b034ab25b219436eb2
Guillermo Valle <guillefix@gmail.com>
2016-11-20 01:32:54 +0000

    sections

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -11,9 +11,10 @@ import { People } from '../api/collections.js';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {section: "content"};
   }
-
   render() {
+    let thisApp = this;
     return (
       <div>
       <nav className="navbar navbar-default" role="navigation">
@@ -28,10 +29,18 @@ class App extends Component {
 
   <div className="collapse navbar-collapse navbar-ex1-collapse">
     <ul className="nav navbar-nav">
-      <li><a href="#">Content</a></li>
-      <li><a href="#">People</a></li>
-      <li><a href="#">Event</a></li>
-      <li><a href="#">Tools</a></li>
+      <li><a onClick={() => {
+        thisApp.setState({section: "content"});
+      }}>Content</a></li>
+      <li><a onClick={() => {
+        thisApp.setState({section: "people"});
+      }}>People</a></li>
+      <li><a onClick={() => {
+        thisApp.setState({section: "event"});
+      }}>Event</a></li>
+      <li><a onClick={() => {
+        thisApp.setState({section: "tools"});
+      }}>Tools</a></li>
     </ul>
   </div>
   <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -48,14 +57,24 @@ class App extends Component {
       </form>
       </div>
   <div>
-  {this.props.people.map((person) => (
-      <Person key={person._id} person={person} />
-    ))}
+    { if (this.state.section === "contents") {
+      <List section={this.state.section} results={}/>
+    } else (this.state.section === "people") {
+      <List section={this.state.section} results={}/>
+    } else (this.state.section === "events") {
+      <List section={this.state.section} results={}/>
+    } else (this.state.section === "tools") {
+      <List section={this.state.section} results={}/>
+    }}
   </div>
 </div>
     );
   }
 }
+
+// {this.props.people.map((person) => (
+//     <Person key={person._id} person={person} />
+//   ))}
 
 App.propTypes = {
   currentUser: PropTypes.object,
@@ -65,16 +84,26 @@ App.propTypes = {
 class Person extends Component {
   render() {
     return (
-      <div>
-        {this.props.person.name}
+      <div className="person-box">
+        <p>Name: {this.props.person.name}</p>
+        <p>Location: {this.props.person.location}</p>
+        <p>Interests: {this.props.person.interests.map(interest=>(<span>{interest}, </span>))}</p>
+        <p>Sills: {this.props.person.skills.map(skill=>(<span>{skill}, </span>))}</p>
       </div>
     )
   }
 }
 
 export default createContainer(() => {
-  console.log(People.find({}).fetch());
+  People.insert({"name": "I am a person", "location": "Not here", "interests": ["Trains", "Turtles"], "skills": ["Javascript", "Html"]});
+  People.insert({"name": "Person Peterson", "location": "Thereburg", "interests": ["IA", "AI"], "skills": ["Making clay forge blower", "Computatoinal learning theory"]});
+
+  // console.log(People.find({}).fetch());
   return {
-    people: People.find({}).fetch()
+    content: Posts.find({}).fetch(),
+    people: People.find({}).fetch(),
+    events: Events.find({}).fetch(),
+    tools: Tool.find({}).fetch(),
+
   };
 }, App);
